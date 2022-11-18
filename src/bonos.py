6a8b8cd22b8ef48ba0f24382be6b5336f277aaba
lmpizarro <lmpizarro@gmail.com>
2022-11-18 19:01:53 -0300

    cupon positions

--- src/bonos.py
+++ src/bonos.py
@@ -20,25 +20,29 @@ bon[TIME] = np.linspace(1/pays_per_year, years, pays_per_year * years)
 # bon[0] = np.exp(-bon[4] * .04)
 bon[DISC_FACTOR] = np.power(1 + interest / pays_per_year, -pays_per_year*bon[TIME])
 
-
 bon[5] = (bon[1] + bon[3]) * bon[0]
 
 
 print(bon)
 
 print(bon[5].sum())
-p = np.zeros(years * DAYS_IN_A_YEAR - 1)
-for i in range(years * DAYS_IN_A_YEAR - 1):
+
+price = np.zeros(years * DAYS_IN_A_YEAR - 1)
+positions = [p for p in range(180, price.shape[0], 180)]
+cupon_acc = np.zeros(years * DAYS_IN_A_YEAR - 1)
+np.put(cupon_acc, positions, .2)
+for i in range(int(.2 * years * DAYS_IN_A_YEAR) - 1):
     bon[4] -= DAY
     mask = np.where(bon[4] < 1e-6 , 0, 1)
     bon[0] = np.power(1 + .04 / 2, -2*bon[4]) * mask
     bon[5] = (bon[1] + bon[3]) * bon[0]
-    p[i] =  bon[5].sum()
+    price[i] =  bon[5].sum()
 
-print(bon)
+print(cupon_acc)
+exit()
 print(bon[5].sum())
 
 import matplotlib.pyplot as plt
-plt.plot(p)
+plt.plot(price)
 plt.show()
 print(p.shape)
