563a1cb6274d0d372fdd0467f3c83c48a8d9dbf6
lmpizarro <lmpizarro@gmail.com>
2022-11-20 20:19:15 -0300

    duration

--- src/bonos.py
+++ src/bonos.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 DAYS_IN_A_YEAR = 364
 DAY = 1 / DAYS_IN_A_YEAR
 
 
-N_ROWS = 6
+N_ROWS = 7
 ROW_DISC_FACTOR = 0
 ROW_COUPON = 1
 ROW_AMORT = 3
@@ -25,31 +26,27 @@ bon[ROW_DISC_FACTOR] = np.power(1 + interest / pays_per_year, -pays_per_year*bon
 bon[ROW_PV] = (bon[ROW_COUPON] + bon[ROW_AMORT]) * bon[ROW_DISC_FACTOR]
 print(bon)
 
-print(bon[5].sum())
-
-def simulation():
+def simulation(rate: float = .04):
     price = np.zeros(years * DAYS_IN_A_YEAR - 1)
+    durt = np.zeros(years * DAYS_IN_A_YEAR - 1)
     positions = [p for p in range(int(DAYS_IN_A_YEAR/pays_per_year), price.shape[0], int(DAYS_IN_A_YEAR/pays_per_year))]
     cupon_acc = np.zeros(years * DAYS_IN_A_YEAR - 1)
     np.put(cupon_acc, positions, interest / pays_per_year)
-
-    print(cupon_acc)
-
+    bon_c = np.copy(bon)
     for t in range(int( years * DAYS_IN_A_YEAR) - 1):
-        bon[ROW_COUPON] *= (1 + cupon_acc[t])
-        bon[ROW_AMORT] *= (1 + cupon_acc[t])
-        bon[ROW_TIME] -= DAY
-        mask = np.where(bon[ROW_TIME] < 1e-6 , 0, 1)
-        bon[ROW_DISC_FACTOR] = np.power(1 + .04 / pays_per_year, -pays_per_year * bon[ROW_TIME]) * mask
-        bon[ROW_PV] = (bon[ROW_COUPON] + bon[ROW_AMORT]) * bon[ROW_DISC_FACTOR]
-        price[t] =  bon[ROW_PV].sum()
+        bon_c[ROW_COUPON] *= (1 + cupon_acc[t])
+        bon_c[ROW_AMORT] *= (1 + cupon_acc[t])
+        bon_c[ROW_TIME] -= DAY
+        mask = np.where(bon_c[ROW_TIME] < 1e-6 , 0, 1)
+        bon_c[ROW_DISC_FACTOR] = np.power(1 + rate / pays_per_year, -pays_per_year * bon_c[ROW_TIME]) * mask
+        bon_c[ROW_PV] = (bon_c[ROW_COUPON] + bon_c[ROW_AMORT]) * bon_c[ROW_DISC_FACTOR]
+        durt[t] = (bon_c[ROW_PV] * bon_c[ROW_TIME] * mask).sum()
+        price[t] =  bon_c[ROW_PV].sum()
+    plt.plot(durt)
+    plt.show()
     return price
 
-price = simulation()
+prices = simulation(.5)
 
-print(bon[ROW_PV].sum())
 
-import matplotlib.pyplot as plt
-plt.plot(price)
-plt.show()
-print(price.shape)
+
